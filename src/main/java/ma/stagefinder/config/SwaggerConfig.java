5dac5231b77d2d2c60e5474bd50ef7ad9afd6d51
MALKI Mohammed <malki.mohammed.1997@gmail.com>
2025-04-28 17:01:43 +0200

    feat:added swagger configuration

---
+++ src/main/java/ma/stagefinder/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Stage Finder API")
+                        .description("Stage finder API")
+                        .contact(new Contact().name("seomaniak"))
+                        .version("1.0"));
+    }
+}
---
+++ src/main/java/ma/stagefinder/controllers/TestController.java
@@ -0,0 +1,13 @@
+package ma.stagefinder.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
